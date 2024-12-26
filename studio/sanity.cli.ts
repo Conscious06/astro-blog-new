2ac7636fd2676539207eb47c29b5011be166fa91
Conscious06 <159427626+Conscious06@users.noreply.github.com>
2024-12-26 13:33:05 +0300

    Update sanity.cli.ts

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -8,7 +8,7 @@
 import {defineCliConfig} from 'sanity/cli'
 
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '<your project ID>'
-const dataset = process.env.SANITY_STUDIO_DATASET || 'dalyanguider'
+const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
 
 export default defineCliConfig({
   api: {
